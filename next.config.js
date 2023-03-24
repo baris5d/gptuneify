eb10a5647d73610e695dc53a94e1bf1efae4ec27
baris5d <baris@barisdede.com>
2023-03-24 16:11:00 +0300

    init

--- next.config.js
+++ next.config.js
@@ -1,8 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-}
+    env: {
+        SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
+        SPOTIFY_REDIRECT_URI: process.env.SPOTIFY_REDIRECT_URI,
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "i.scdn.co",
+                pathname: "/image/*",
+                port: "",
+            },
+        ],
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
